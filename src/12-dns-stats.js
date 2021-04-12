4fa58149c2d53fc0f518f15e853ebc0cb933f636
nadezhdaGol <78170166+nadezhdaGol@users.noreply.github.com>
2021-04-12 19:29:32 +0300

    feat: add solution of the task 12-dns-stats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,46 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const shortDomains = [];
+
+  for (let i = 0; i < domains.length; i++) {
+    for (let j = domains[i].length - 1; j >= 0; j--) {
+      if (domains[i][j] === '.') {
+        shortDomains.push(domains[i].slice(j + 1));
+      }
+    }
+  }
+
+  const allDomains = domains.concat(shortDomains);
+  const uniqueDomains = Array.from(new Set(allDomains));
+
+  let finDomains = [];
+  for (let i = 0; i < uniqueDomains.length; i++) {
+    finDomains.push(`.${uniqueDomains[i].split('.').reverse().join('.')}`);
+  }
+  finDomains = finDomains.sort();
+
+  const originalDomainsRev = [];
+  for (let i = 0; i < domains.length; i++) {
+    originalDomainsRev.push(`.${domains[i].split('.').reverse().join('.')}`);
+  }
+
+  const countDomains = [];
+  for (let i = 0; i < originalDomainsRev.length; i++) {
+    for (let j = 0; j < finDomains.length; j++) {
+      if (originalDomainsRev[i].includes(finDomains[j])) {
+        countDomains.push(finDomains[j]);
+      }
+    }
+  }
+
+  const resObj = {};
+  countDomains.forEach((item) => {
+    resObj[item] = (resObj[item] || 0) + 1;
+  });
+
+  return resObj;
 }
 
 module.exports = getDNSStats;
